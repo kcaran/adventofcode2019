3ddac120d3bc29f7e22ffae8700262db51fdcf3e
Keith Carangelo <keithcarangelo@safetyinsurance.com>
2019-12-15 10:33:11 -0500

    Finished Day 14

--- day14.pl
+++ day14.pl
@@ -61,8 +61,6 @@ use POSIX qw( ceil );
     my $self = {
       react => [],
       out => {},
-      ore => 0,
-      left => {},
     };
 
    for (Path::Tiny::path( $input_file )->lines_utf8( { chomp => 1 } )) {
@@ -82,8 +80,29 @@ use POSIX qw( ceil );
 my $input_file = $ARGV[0] || 'input14.txt';
 
 my $fact = Nanofactory->new( $input_file );
+my $amount = $ARGV[1] || 1;
 
 my $make = $fact->make( { FUEL => 1 } );
-print "It takes ", $make->{ ORE }, " ORE to make fuel\n";
+if ($amount == 1) {
+  # Part A
+  my $make = $fact->make( { FUEL => $amount } );
+  print "It takes ", $make->{ ORE }, " ORE to make fuel\n";
+ }
+else {
+  # Part B
+  my $one_fuel = $make->{ ORE };
+  my $fuel = int( $amount / $one_fuel );
+  my $ore;
+  do {
+    $make = $fact->make( { FUEL => $fuel } );
+    $ore = $amount - $make->{ ORE };
+    $fuel += int( $ore / $one_fuel );
+  } while ($ore >= $one_fuel);
 
+  # We are within one fuel unit - keep trying
+  while ($fact->make( { FUEL => $fuel + 1 } )->{ ORE } < $amount) {
+    $fuel++;
+   }
+  print "You can make $fuel with $amount ORE\n";
+ }
 exit;
