52301091b9f1e877aa481efcda07ca4a57d788b5
Keith Carangelo <mail@kcaran.com>
2023-01-20 14:06:38 -0500

    Test 18h passes but input is too slow

--- day18b.pl
+++ day18b.pl
@@ -1,34 +1,8 @@
 #!/usr/bin/env perl
 #
-# I had to cheat a little and look at reddit:
-#
-# https://www.reddit.com/r/adventofcode/comments/ecsw02/2019_day_18_missing_a_critical_step_of_the/
-#
-# You can think about this as a dynamic programming problem, with the
-# state being (x, y, keys collected).
-#
-# You start at a particular bot position with 0 keys, i.e. your state is
-# (bx, by, 0). You want to find the best possible score from this state,
-# which we'll call score(bx, by, 0) (representing keys as a bitfield).
-# There are a set of keys that you can reach, and the best score across
-# all reachable keys is something like
-#
-# def score(x, y, keys):
-#   return min([score(key_x, key_y, new_key | keys) + steps_to(key_x, key_y)
-#                for (key_x, key_y, new_key) in reachable(x, y, keys)])
-# Notice that this calls score recursively; you'll want to memoize calls
-# to score to cut down on evaluation time.
-#
-# My problem was that I was trying to update the map after every move,
-# cloning the object again and again with each found key and
-# each open door.
-#
-# I was able to get the test18d.txt down but the puzzle input still took
-# a long time:
-#
-# ..such that a lot of times moving from key point a to b takes you via
-# multiple other keys, which further cuts down on possible state
-# combinations at each end.
+# I had to re-write this for part b - I made it simpler to make it
+# pass the tests, but for the input taking a single step in each
+# direction was much much too slow!
 #
 use strict;
 use warnings;
@@ -41,51 +15,7 @@ my $map;
 
 my $state;
 
-sub min_path {
-  my $num_keys = 0;
-  my $path = 0;
-  for my $k (keys %{ $state }) {
-    my $len = length( $k );
-    if ($len > $num_keys) {
-      $num_keys = $len;
-      $path = $state->{ $k };
-     }
-    elsif ($len == $num_keys && $path > $state->{ $k }) {
-      $path = $state->{ $k };
-     }
-   }
-  return $path;
- }
-
 { package Map;
-
-  sub next_keys {
-    my ($self, $initial) = @_;
-
-    $self->{ prev_moves } = {};
-    my @moves = $map->valid_moves( $initial );
-    my @keys = ();
-
-    for my $move (@moves) {
-      my (@y, @x, $cnt, $keys);
-      ($y[0], $x[0], $y[1], $x[1], $y[2], $x[2], $y[3], $x[3], $cnt, $keys) = split( ',', $move );
-    for my $bot (0 .. 3) {
-      my $point = $self->{ map }[$y[$bot]][$x[$bot]];
-      if ($point ge 'a' && $point le 'z' && index( $keys, $point ) < 0) {
-        $keys = $point . join( '', sort split( '', $keys ) );
-        if (!$state->{ $keys } || $state->{ $keys } > $cnt) {
-          my $pos = join( ',', map{ "$y[$_],$x[$_]" } 0 .. 3 );
-          push (@keys, "$pos,$cnt,$keys");
-          $state->{ $keys } = $cnt;
-         }
-        next;
-       }
-      push @moves, $map->valid_moves( $move );
-     }
-     }
-
-    return @keys;
-   }
 
   sub valid_moves {
     my ($self, $move) = @_;
@@ -95,6 +25,7 @@ sub min_path {
     ($y[0], $x[0], $y[1], $x[1], $y[2], $x[2], $y[3], $x[3], $cnt, $keys) = split( ',', $move );
     $cnt++;
 
+    return @moves if ($self->{ min_path } > 0 && $cnt > $self->{ min_path });
     for my $bot (0 .. 3) {
     for my $dir ([ 0, 1 ], [ 0, -1 ], [ -1, 0 ], [ 1, 0 ]) {
       my @new_y = @y;
@@ -107,10 +38,21 @@ sub min_path {
       next if (($point ge 'A' && $point le 'Z') && index( $keys, lc( $point )) < 0);
 
       my $new_pos = join( ',', map{ "$new_y[$_],$new_x[$_]" } 0 .. 3 );
-      next if ($self->{ prev_moves }{ $new_pos });
-      $self->{ prev_moves }{ $new_pos } = 1;
+      next if ($self->{ prev_moves }{ "$new_pos,$cnt,$keys" });
 
-      push @moves, "$new_pos,$cnt,$keys";
+      my $new_keys = $keys;
+      if ($point ge 'a' && $point le 'z' && index( $keys, $point ) < 0) {
+        $new_keys = join( '', sort split( '', "${keys}${point}" ) );
+        $state->{ $new_keys } = $cnt unless ($state->{ $new_keys } && $state->{ $new_keys } <= $cnt);
+        if (length( $new_keys ) == keys %{ $self->{ key } }) {
+          if ($self->{ min_path } < 0 || $self->{ min_path } > $cnt) {
+            $self->{ min_path } = $cnt;
+           }
+         }
+       }
+
+      push @moves, "$new_pos,$cnt,$new_keys";
+      $self->{ prev_moves }{ "$new_pos,$cnt,$new_keys" } = 1;
      }
      }
 
@@ -128,6 +70,8 @@ sub min_path {
     my $self = {
      map => [],
      key => {},
+     min_path => -1,
+     prev_moves => {},
     };
 
     my ($x, $y) = (0, 0);
@@ -148,6 +92,8 @@ sub min_path {
       $y++;
      }
 
+    $self->{ num_keys } = keys %{ $self->{ keys } };
+
     # Update map for part b
     ($y, $x) = @start;
     $self->{ map }[$y-1][$x] = '#';
@@ -165,14 +111,14 @@ my $input_file = $ARGV[0] || 'input18.txt';
 
 $map = Map->new( $input_file );
 
-my @moves = $map->next_keys( $map->start() );
+my @moves = ( $map->start() );
 
 while (@moves) {
   my $move = shift @moves;
-  print "For $move, there are now ", scalar( @moves ), " moves\n";
-  push @moves, $map->next_keys( $move );
+  print "For $move there are now ", scalar( @moves ), " moves\n";
+  push @moves, $map->valid_moves( $move );
  }
 
-print "We found all the keys in ", min_path(), " moves.\n";
+print "We found all the keys in $map->{ min_path } moves.\n";
 
 exit;
