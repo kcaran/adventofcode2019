784c84d218d156fe0862e15ba3e25976026d8aa8
Keith Carangelo <mail@kcaran.com>
2023-01-20 16:28:50 -0500

    Day 18b tests pass but input is too slow. :-(

--- day18b.pl
+++ day18b.pl
@@ -4,6 +4,9 @@
 # pass the tests, but for the input taking a single step in each
 # direction was much much too slow!
 #
+# Next is that I was memoizing with the counts - We should only include
+# the minimum steps to get to that position with keys!
+#
 use strict;
 use warnings;
 use utf8;
@@ -22,37 +25,62 @@ my $state;
     my @moves = ();
 
     my (@y, @x, $cnt, $keys);
-    ($y[0], $x[0], $y[1], $x[1], $y[2], $x[2], $y[3], $x[3], $cnt, $keys) = split( ',', $move );
-    $cnt++;
+    ($y[0], $x[0], $y[1], $x[1], $y[2], $x[2], $y[3], $x[3], $keys) = split( ',', $move );
+    $keys ||= '';
+    $cnt = $self->{ prev_moves }{ $move } + 1;
 
     return @moves if ($self->{ min_path } > 0 && $cnt > $self->{ min_path });
     for my $bot (0 .. 3) {
     for my $dir ([ 0, 1 ], [ 0, -1 ], [ -1, 0 ], [ 1, 0 ]) {
       my @new_y = @y;
       my @new_x = @x;
+      my $new_cnt = $cnt;
       $new_y[$bot] = $y[$bot] + $dir->[0];
       $new_x[$bot] = $x[$bot] + $dir->[1];
 
       my $point = $self->{ map }[$new_y[$bot]][$new_x[$bot]];
       next if ($point eq '#');
+
+      # Move in the same direction if a hallway
+      if ($dir->[0] == 0) {
+        while ($self->{ map }[$y[$bot] - 1][$new_x[$bot]] eq '#'
+            && $self->{ map }[$y[$bot] + 1][$new_x[$bot]] eq '#'
+            && $self->{ map }[$y[$bot]][$new_x[$bot]+$dir->[1]] eq '.') {
+          $new_x[$bot] += $dir->[1];
+          $new_cnt++;
+         }
+       }
+      else {
+        while ($self->{ map }[$new_y[$bot]][$x[$bot] - 1] eq '#'
+            && $self->{ map }[$new_y[$bot]][$x[$bot] + 1] eq '#'
+            && $self->{ map }[$new_y[$bot]+$dir->[0]][$x[$bot]] eq '.') {
+          $new_y[$bot] += $dir->[0];
+          $new_cnt++;
+         }
+       }
+
       next if (($point ge 'A' && $point le 'Z') && index( $keys, lc( $point )) < 0);
 
       my $new_pos = join( ',', map{ "$new_y[$_],$new_x[$_]" } 0 .. 3 );
-      next if ($self->{ prev_moves }{ "$new_pos,$cnt,$keys" });
+      next if ($self->{ prev_moves }{ "$new_pos,$keys" }
+			&& $self->{ prev_moves }{ "$new_pos,$keys" } <= $new_cnt);
 
       my $new_keys = $keys;
       if ($point ge 'a' && $point le 'z' && index( $keys, $point ) < 0) {
         $new_keys = join( '', sort split( '', "${keys}${point}" ) );
-        $state->{ $new_keys } = $cnt unless ($state->{ $new_keys } && $state->{ $new_keys } <= $cnt);
+        if (!$state->{ $new_keys } || $state->{ $new_keys } > $new_cnt) {
+          $state->{ $new_keys } = $cnt;
+          print "$new_keys == $cnt\n";
+         }
         if (length( $new_keys ) == keys %{ $self->{ key } }) {
-          if ($self->{ min_path } < 0 || $self->{ min_path } > $cnt) {
-            $self->{ min_path } = $cnt;
+          if ($self->{ min_path } < 0 || $self->{ min_path } > $new_cnt) {
+            $self->{ min_path } = $new_cnt;
            }
          }
        }
 
-      push @moves, "$new_pos,$cnt,$new_keys";
-      $self->{ prev_moves }{ "$new_pos,$cnt,$new_keys" } = 1;
+      push @moves, "$new_pos,$new_keys";
+      $self->{ prev_moves }{ "$new_pos,$new_keys" } = $new_cnt;
      }
      }
 
@@ -100,7 +128,7 @@ my $state;
     $self->{ map }[$y+1][$x] = '#';
     $self->{ map }[$y][$x-1] = '#';
     $self->{ map }[$y][$x+1] = '#';
-    $self->{ start } = join( ',', $y-1, $x-1, $y+1, $x-1, $y-1, $x+1, $y+1, $x+1, 0, undef );
+    $self->{ start } = join( ',', $y-1, $x-1, $y+1, $x-1, $y-1, $x+1, $y+1, $x+1, undef );
 
     bless $self, $class;
     return $self;
@@ -115,7 +143,7 @@ my @moves = ( $map->start() );
 
 while (@moves) {
   my $move = shift @moves;
-  print "For $move there are now ", scalar( @moves ), " moves\n";
+# print "For $move there are now ", scalar( @moves ), " moves\n";
   push @moves, $map->valid_moves( $move );
  }
 
