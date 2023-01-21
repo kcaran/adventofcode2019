e42ece756b98ecde4899dcbdece73c96bf1a3964
Keith Carangelo <mail@kcaran.com>
2023-01-20 20:11:38 -0500

    Finished Day 18!

--- day18b.pl
+++ day18b.pl
@@ -1,18 +1,15 @@
 #!/usr/bin/env perl
 #
-# I had to re-write this for part b - I made it simpler to make it
-# pass the tests, but for the input taking a single step in each
-# direction was much much too slow!
-#
-# Next is that I was memoizing with the counts - We should only include
-# the minimum steps to get to that position with keys!
+# I thought I did this wrong when I came back to it in 2023. But it
+# turned out I had the right idea: for each bot, find all the keys
+# you can see. I think it could be faster if I did better caching
+# of previous results, but this works!
 #
 use strict;
 use warnings;
 use utf8;
 
 use Path::Tiny;
-use Storable 'dclone';
 
 my $map;
 
@@ -20,68 +17,66 @@ my $state;
 
 { package Map;
 
-  sub valid_moves {
-    my ($self, $move) = @_;
-    my @moves = ();
+  sub next_keys {
+    my ($self, $initial, $bot) = @_;
+    my @key_moves;
+
+    $self->{ prev_moves } = {};
+    my @moves = ( $initial );
 
     my (@y, @x, $cnt, $keys);
-    ($y[0], $x[0], $y[1], $x[1], $y[2], $x[2], $y[3], $x[3], $keys) = split( ',', $move );
+    ($y[0], $x[0], $y[1], $x[1], $y[2], $x[2], $y[3], $x[3], $keys, $cnt) = split( ',', $initial );
     $keys ||= '';
-    $cnt = $self->{ prev_moves }{ $move } + 1;
 
-    return @moves if ($self->{ min_path } > 0 && $cnt > $self->{ min_path });
-    for my $bot (0 .. 3) {
-    for my $dir ([ 0, 1 ], [ 0, -1 ], [ -1, 0 ], [ 1, 0 ]) {
-      my @new_y = @y;
-      my @new_x = @x;
-      my $new_cnt = $cnt;
-      $new_y[$bot] = $y[$bot] + $dir->[0];
-      $new_x[$bot] = $x[$bot] + $dir->[1];
+    @moves = ( "$y[$bot],$x[$bot],$keys,$cnt" );
 
-      my $point = $self->{ map }[$new_y[$bot]][$new_x[$bot]];
-      next if ($point eq '#');
+    while (@moves) {
+      my $move = shift @moves;
 
-      # Move in the same direction if a hallway
-      if ($dir->[0] == 0) {
-        while ($self->{ map }[$y[$bot] - 1][$new_x[$bot]] eq '#'
-            && $self->{ map }[$y[$bot] + 1][$new_x[$bot]] eq '#'
-            && $self->{ map }[$y[$bot]][$new_x[$bot]+$dir->[1]] eq '.') {
-          $new_x[$bot] += $dir->[1];
-          $new_cnt++;
-         }
-       }
-      else {
-        while ($self->{ map }[$new_y[$bot]][$x[$bot] - 1] eq '#'
-            && $self->{ map }[$new_y[$bot]][$x[$bot] + 1] eq '#'
-            && $self->{ map }[$new_y[$bot]+$dir->[0]][$x[$bot]] eq '.') {
-          $new_y[$bot] += $dir->[0];
-          $new_cnt++;
-         }
-       }
-
-      next if (($point ge 'A' && $point le 'Z') && index( $keys, lc( $point )) < 0);
-
-      my $new_pos = join( ',', map{ "$new_y[$_],$new_x[$_]" } 0 .. 3 );
-      next if ($self->{ prev_moves }{ "$new_pos,$keys" }
-			&& $self->{ prev_moves }{ "$new_pos,$keys" } <= $new_cnt);
-
-      my $new_keys = $keys;
+      my ($y, $x, $keys, $cnt) = split( ',', $move );
+      my $point = $self->{ map }[$y][$x];
       if ($point ge 'a' && $point le 'z' && index( $keys, $point ) < 0) {
-        $new_keys = join( '', sort split( '', "${keys}${point}" ) );
-        if (!$state->{ $new_keys } || $state->{ $new_keys } > $new_cnt) {
+        $y[$bot] = $y;
+        $x[$bot] = $x;
+        my $new_pos = join( ',', map{ "$y[$_],$x[$_]" } 0 .. 3 );
+        my $new_keys = join( '', sort split( '', "${keys}${point}" ) );
+
+        if (!$state->{ $new_keys } || $state->{ $new_keys } > $cnt) {
           $state->{ $new_keys } = $cnt;
           print "$new_keys == $cnt\n";
          }
         if (length( $new_keys ) == keys %{ $self->{ key } }) {
-          if ($self->{ min_path } < 0 || $self->{ min_path } > $new_cnt) {
-            $self->{ min_path } = $new_cnt;
+          if ($self->{ min_path } < 0 || $self->{ min_path } > $cnt) {
+            $self->{ min_path } = $cnt;
            }
          }
+        push @key_moves, "$new_pos,$new_keys,$cnt";
+        next;
        }
-
-      push @moves, "$new_pos,$new_keys";
-      $self->{ prev_moves }{ "$new_pos,$new_keys" } = $new_cnt;
+      push @moves, $self->valid_moves( $move );
      }
+
+    return @key_moves;
+   }
+
+  sub valid_moves {
+    my ($self, $move) = @_;
+    my @moves = ();
+
+    my ($y, $x, $keys, $cnt) = split( ',', $move );
+    $cnt++;
+    for my $dir ([ 0, 1 ], [ 0, -1 ], [ -1, 0 ], [ 1, 0 ]) {
+      my $new_y = $y + $dir->[0];
+      my $new_x = $x + $dir->[1];
+
+      my $new_pos = "$new_y,$new_x";
+      next if ($self->{ prev_moves }{ $new_pos });
+      $self->{ prev_moves }{ $new_pos } = 1;
+
+      my $point = $self->{ map }[$new_y][$new_x];
+      next if ($point eq '#');
+      next if (($point ge 'A' && $point le 'Z') && index( $keys, lc( $point )) < 0);
+      push @moves, "$new_pos,$keys,$cnt";
      }
 
     return @moves;
@@ -99,7 +94,6 @@ my $state;
      map => [],
      key => {},
      min_path => -1,
-     prev_moves => {},
     };
 
     my ($x, $y) = (0, 0);
@@ -108,7 +102,7 @@ my $state;
       $x = 0;
       for my $col (split( '', $row )) {
         if ($col eq '@') {
-          @start = ( $y, $x );
+          @start  = ( $y, $x );
           $col = '.';
          }
         elsif ($col ge 'a' && $col le 'z') {
@@ -128,7 +122,7 @@ my $state;
     $self->{ map }[$y+1][$x] = '#';
     $self->{ map }[$y][$x-1] = '#';
     $self->{ map }[$y][$x+1] = '#';
-    $self->{ start } = join( ',', $y-1, $x-1, $y+1, $x-1, $y-1, $x+1, $y+1, $x+1, undef );
+    $self->{ start } = join( ',', $y-1, $x-1, $y+1, $x-1, $y-1, $x+1, $y+1, $x+1, undef, 0 );
 
     bless $self, $class;
     return $self;
@@ -139,12 +133,18 @@ my $input_file = $ARGV[0] || 'input18.txt';
 
 $map = Map->new( $input_file );
 
+my %seen;
 my @moves = ( $map->start() );
-
 while (@moves) {
   my $move = shift @moves;
-# print "For $move there are now ", scalar( @moves ), " moves\n";
-  push @moves, $map->valid_moves( $move );
+  my @split = split( ',', $move );
+  my $cnt = pop @split;
+  my $key = join( ',', @split );
+  next if ($seen{ $key } && $seen{ $key } <= $cnt);
+  $seen{ $key } = $cnt;
+  for my $bot (0 .. 3) {
+    push @moves, $map->next_keys( $move, $bot );
+   }
  }
 
 print "We found all the keys in $map->{ min_path } moves.\n";
